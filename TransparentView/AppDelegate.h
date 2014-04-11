15d754855522946f50fb754840f90161f735f243
fila95 <fila95@live.it>
2014-04-11 18:25:00 +0200

    Initial Commit

---
+++ TransparentView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TransparentView
+//
+//  Created by Giovanni Filaferro on 11/04/14.
+//  Copyright (c) 2014 Giovanni Filaferro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TransparentView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TransparentView
+//
+//  Created by Giovanni Filaferro on 11/04/14.
+//  Copyright (c) 2014 Giovanni Filaferro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
